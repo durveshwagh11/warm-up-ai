90d27c149909782dc9c969e7c8bd3b936ee6a081
Durvesh Wagh <durveshwagh11@gmail.com>
2024-06-19 10:10:29 +0530

    comment

--- app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
@@ -1,7 +1,15 @@
-import { Lightbulb } from 'lucide-react';
+import { Lightbulb, Volume2 } from 'lucide-react';
 import React from 'react';
 
 function QuestionSection({ mockInterviewQuestion, activeQuestionIndex, setActiveQuestionIndex }) {
+    const textToSpeech = (text) => {
+        if('speechSynthesis' in window) {
+            const speech = new SpeechSynthesisUtterance(text);
+            window.speechSynthesis.speak(speech);
+        } else {
+            alert('Web Speech API is not supported in this browser.');
+        }
+    };
     return (
         <div className='p-5 border rounded-lg my-10'>
             {mockInterviewQuestion && mockInterviewQuestion.length > 0 ? (
@@ -18,6 +26,7 @@ function QuestionSection({ mockInterviewQuestion, activeQuestionIndex, setActive
                         ))}
                     </div>
                     <h2 className='my-5 text-md md:text-lg'>{mockInterviewQuestion[activeQuestionIndex]?.question}</h2>
+                    <Volume2 className='cursor-pointer'onClick={() => textToSpeech(mockInterviewQuestion[activeQuestionIndex]?.question)}/>
                     <div className='border rounded-lg p-5 bg-blue-100 mt-20'>
                         <h2 className='flex gap-2 items-center text-blue-500'>
                             <Lightbulb/>
