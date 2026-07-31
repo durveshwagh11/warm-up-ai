4c0da015d9b7fee7880021c25104484de22c55ad
durveshwagh11 <durveshwagh11@gmail.com>
2026-07-31 17:09:03 +0530

    Add Backend Setup Changes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { InterviewModule } from './interview/interview.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [InterviewModule],
+  imports: [
+    InterviewModule,
+    AuthModule,
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    RedisModule,
+  ],
 })
 export class AppModule {}
--- backend/src/database/database.service.ts
+++ backend/src/database/database.service.ts
@@ -2,13 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
+import 'dotenv/config';
 
 @Injectable()
 export class DatabaseService {
   public db;
 
   constructor() {
-    const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB_URL);
+    const sql = neon(process.env.DATABASE_URL!);
     this.db = drizzle(sql, { schema });
+
+    this.testConnection().catch((err) => {
+      console.error('Database connection failed:', err);
+    });
+  }
+
+  private async testConnection() {
+    const result = await this.db.execute('SELECT NOW()');
+    console.log('Database connected:', result.rows[0]);
   }
 }
